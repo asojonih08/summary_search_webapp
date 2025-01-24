529e167acd4469df3f2ce3855e7562b2e98c3f84
asojonih08 <27hinojosa.r@gmail.com>
2025-01-23 22:59:21 -0800

    Adjust getSummary 2

--- actions/getSummary.ts
+++ actions/getSummary.ts
@@ -8,7 +8,7 @@ export async function getSummary(
   }
 
   try {
-    const response = await fetch("http://192.168.86.35:5000/api", {
+    const response = await fetch("http://192.168.86.35:8080", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
