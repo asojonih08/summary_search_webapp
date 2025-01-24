53f8a43d70779b0be54cef23bb58f0fef3358039
asojonih08 <27hinojosa.r@gmail.com>
2025-01-23 22:50:48 -0800

    Changed home page layout so input won't shift when suggestions open

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SearchCard from "@/components/SearchCard";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-full flex flex-col justify-center items-center">
+    <div className="w-full min-h-full flex flex-col  items-center pt-[20vh]">
       <h3 className="dark:text-textMainDark text-4xl mb-10">
         What do you want to know?
       </h3>
