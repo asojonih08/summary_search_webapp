20653e34a56877486940fd38c68ba48d5a1a0b4a
r_hinojosa <r_hinojosa@ucsb.edu>
2025-02-09 20:24:28 -0800

    react markdown paragraph elements are now rendered in span instead of p tags to avoid nesting tooltip div inside p element

--- components/MemoizedMarkdown.tsx
+++ components/MemoizedMarkdown.tsx
@@ -5,7 +5,6 @@ import remarkGfm from "remark-gfm"; // Enables GitHub-style Markdown (tables, li
 
 import SourceCircleTooltip from "@/components/SourceCircleTooltip";
 import { SearchResult } from "@/actions/getSearchResults";
-import SearchResults from "./SearchResults";
 
 // Define props for Citation component
 interface CitationProps {
@@ -19,13 +18,15 @@ const Citation = ({ sourceNumber, searchResults }: CitationProps) => {
     // <span className="dark:text-textMainDark text-xs cursor-pointer hover:underline rounded-full h-6 w-6 p-2 mx-0.5 bg-offsetPlusDark inline-flex items-center justify-center">
     //   <span className="">{numbers}</span>
     // </span>
-    <SourceCircleTooltip
-      sourceNumber={Number(sourceNumber)}
-      link={searchResults[sourceNumber - 1].link ?? ""}
-      displayLink={searchResults[sourceNumber - 1].displayLink ?? ""}
-      snippet={searchResults[sourceNumber - 1].snippet ?? ""}
-      title={searchResults[sourceNumber - 1].title ?? ""}
-    />
+    <span className="inline-block relative">
+      <SourceCircleTooltip
+        sourceNumber={Number(sourceNumber)}
+        link={searchResults[sourceNumber - 1].link ?? ""}
+        displayLink={searchResults[sourceNumber - 1].displayLink ?? ""}
+        snippet={searchResults[sourceNumber - 1].snippet ?? ""}
+        title={searchResults[sourceNumber - 1].title ?? ""}
+      />
+    </span>
   );
 };
 
@@ -54,81 +55,85 @@ const MemoizedMarkdownBlock = memo(
   ({ content, searchResults }: MemoizedMarkdownBlockProps) => {
     console.log("Content:  ", content);
     return (
-      <div className="prose prose-p:dark:text-textMainDark prose-strong:dark:text-textMainDark prose-li:list-outside dark:text-textMainDark">
-        <ReactMarkdown
-          remarkPlugins={[remarkGfm]}
-          components={{
-            p: ({ children }) => {
-              // Flatten children to avoid wrapping inline elements inside <p>
-              const content = Array.isArray(children)
-                ? children.flat()
-                : [children];
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          p: ({ children }) => {
+            // Flatten children to avoid wrapping inline elements inside <p>
+            const content = Array.isArray(children)
+              ? children.flat()
+              : [children];
 
-              return (
-                <p>
-                  {content.map((child, index) => {
-                    if (typeof child === "string") {
-                      const parts = child.split(/(CITATION_\d+)/g);
-                      return parts.map((part, index) => {
-                        if (part === ".") part = part.replace(".", ". ");
-                        return part.startsWith("CITATION_") ? (
+            return (
+              <span className="whitespace-pre-line">
+                {content.map((child) => {
+                  if (typeof child === "string") {
+                    const parts = child.split(/(CITATION_\d+)/g);
+                    return parts.map((part, index) => {
+                      if (part === ".") part = part.replace(".", ". ");
+                      return part.startsWith("CITATION_") ? (
+                        <span className="inline-block" key={index}>
                           <Citation
-                            key={index}
                             sourceNumber={Number(
                               part.replace("CITATION_", "").replace(/_/g, ", ")
                             )}
                             searchResults={searchResults}
                           />
-                        ) : (
-                          part
-                        );
-                      });
-                    }
-                    return child; // Keep existing React elements as-is
-                  })}
-                </p>
-              );
-            },
-            li: ({ children }) => {
-              // console.log("Children: ", children, " \nTYPE: ", typeof children);
+                        </span>
+                      ) : (
+                        part
+                      );
+                    });
+                  }
+                  return child; // Keep existing React elements as-is
+                })}
+              </span>
+            );
+          },
+          li: ({ children }) => {
+            // console.log("Children: ", children, " \nTYPE: ", typeof children);
 
-              // If children is a string, apply citation replacement
-              if (typeof children === "string") {
-                const parts = children.split(/(CITATION_\d+)/g);
-                //   console.log("parts in ReactMarkdown 1: ", parts);
-                return (
-                  <li>
-                    {parts.map((part, index) => {
-                      return part.startsWith("CITATION_") ? (
+            // If children is a string, apply citation replacement
+            if (typeof children === "string") {
+              const parts = children.split(/(CITATION_\d+)/g);
+              //   console.log("parts in ReactMarkdown 1: ", parts);
+              return (
+                <li>
+                  {parts.map((part, index) => {
+                    return part.startsWith("CITATION_") ? (
+                      <span className="inline-block" key={index}>
                         <Citation
-                          key={index}
                           sourceNumber={Number(
                             part.replace("CITATION_", "").replace(/_/g, ", ")
                           )}
                           searchResults={searchResults}
                         />
-                      ) : (
-                        part
-                      );
-                    })}
-                  </li>
-                );
-              }
+                      </span>
+                    ) : (
+                      part
+                    );
+                  })}
+                </li>
+              );
+            }
 
-              // If children is an array, process each part
-              if (Array.isArray(children)) {
-                //   console.log("Children:  ", children);
-                return (
-                  <li>
-                    {children.map((child, index) => {
-                      // If child is a string, apply citation replacement
-                      if (typeof child === "string") {
-                        const parts = child.split(/(CITATION_\d+)/g);
-                        //   console.log("parts in ReactMarkdown 2: ", parts);
-                        return parts.map((part, subIndex) =>
-                          part.startsWith("CITATION_") ? (
+            // If children is an array, process each part
+            if (Array.isArray(children)) {
+              //   console.log("Children:  ", children);
+              return (
+                <li>
+                  {children.map((child, index) => {
+                    // If child is a string, apply citation replacement
+                    if (typeof child === "string") {
+                      const parts = child.split(/(CITATION_\d+)/g);
+                      //   console.log("parts in ReactMarkdown 2: ", parts);
+                      return parts.map((part, subIndex) =>
+                        part.startsWith("CITATION_") ? (
+                          <span
+                            className="inline-block"
+                            key={`${index}-${subIndex}`}
+                          >
                             <Citation
-                              key={`${index}-${subIndex}`}
                               sourceNumber={Number(
                                 part
                                   .replace("CITATION_", "")
@@ -136,66 +141,95 @@ const MemoizedMarkdownBlock = memo(
                               )}
                               searchResults={searchResults}
                             />
-                          ) : (
-                            part
-                          )
-                        );
-                      }
-                      // If child is already a React element (e.g., <strong>), return as-is
-                      return child;
-                    })}
-                  </li>
-                );
-              }
-              console.log(
-                "Other children: ",
-                children,
-                "  TYPE: ",
-                typeof children
+                          </span>
+                        ) : (
+                          part
+                        )
+                      );
+                    }
+                    // If child is already a React element (e.g., <strong>), return as-is
+                    return child;
+                  })}
+                </li>
               );
-              return children;
-            },
-            strong: ({ children }) => (
-              <strong className="underline underline-offset-2 decoration-textOffDark">
-                {children}
-              </strong>
-            ),
-            td: ({ children }) => {
-              // Flatten children to avoid wrapping inline elements inside <p>
-              const content = Array.isArray(children)
-                ? children.flat()
-                : [children];
+            }
+            console.log(
+              "Other children: ",
+              children,
+              "  TYPE: ",
+              typeof children
+            );
+            return children;
+          },
+          strong: ({ children }) => {
+            // Flatten children to avoid wrapping inline elements inside <p>
+            const content = Array.isArray(children)
+              ? children.flat()
+              : [children];
 
-              return (
-                <td>
-                  {content.map((child, index) => {
-                    if (typeof child === "string") {
-                      const parts = child.split(/(CITATION_\d+)/g);
-                      return parts.map((part, index) => {
-                        if (part === ".") part = part.replace(".", ". ");
-                        return part.startsWith("CITATION_") ? (
+            return (
+              <strong>
+                {content.map((child) => {
+                  if (typeof child === "string") {
+                    const parts = child.split(/(CITATION_\d+)/g);
+                    return parts.map((part, index) => {
+                      if (part === ".") part = part.replace(".", ". ");
+                      return part.startsWith("CITATION_") ? (
+                        <span className="inline-block" key={index}>
                           <Citation
-                            key={index}
                             sourceNumber={Number(
                               part.replace("CITATION_", "").replace(/_/g, ", ")
                             )}
                             searchResults={searchResults}
                           />
-                        ) : (
-                          part
-                        );
-                      });
-                    }
-                    return child; // Keep existing React elements as-is
-                  })}
-                </td>
-              );
-            },
-          }}
-        >
-          {content}
-        </ReactMarkdown>
-      </div>
+                        </span>
+                      ) : (
+                        part
+                      );
+                    });
+                  }
+                  return child; // Keep existing React elements as-is
+                })}
+              </strong>
+            );
+          },
+
+          td: ({ children }) => {
+            // Flatten children to avoid wrapping inline elements inside <p>
+            const content = Array.isArray(children)
+              ? children.flat()
+              : [children];
+
+            return (
+              <td>
+                {content.map((child) => {
+                  if (typeof child === "string") {
+                    const parts = child.split(/(CITATION_\d+)/g);
+                    return parts.map((part, index) => {
+                      if (part === ".") part = part.replace(".", ". ");
+                      return part.startsWith("CITATION_") ? (
+                        <span className="inline-block" key={index}>
+                          <Citation
+                            sourceNumber={Number(
+                              part.replace("CITATION_", "").replace(/_/g, ", ")
+                            )}
+                            searchResults={searchResults}
+                          />
+                        </span>
+                      ) : (
+                        part
+                      );
+                    });
+                  }
+                  return child; // Keep existing React elements as-is
+                })}
+              </td>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
     );
   },
   (prevProps, nextProps) => prevProps.content === nextProps.content
@@ -216,11 +250,12 @@ export const MemoizedMarkdown = memo(
     const blocks = useMemo(() => parseMarkdownIntoBlocks(content), [content]);
 
     return blocks.map((block, index) => (
-      <MemoizedMarkdownBlock
-        content={block}
+      <div
         key={`${id}-block_${index}`}
-        searchResults={searchResults}
-      />
+        className="inline text-pretty break-words leading-normal prose prose-p:dark:text-textMainDark prose-strong:dark:text-textMainDark prose-strong:underline prose-strong:underline-offset-2 prose-strong:decoration-textOffDark prose-li:list-outside prose-ol:space-y-6 prose-ol:my-4 dark:text-textMainDark"
+      >
+        <MemoizedMarkdownBlock content={block} searchResults={searchResults} />
+      </div>
     ));
   }
 );
--- components/SourceCircleTooltip.tsx
+++ components/SourceCircleTooltip.tsx
@@ -25,41 +25,37 @@ export default function SourceCircleTooltip({
   const condesedDisplayLink = displayLink.split(".").at(-2);
   const faviconFromGoogle = `https://www.google.com/s2/favicons?domain=${displayLink}&sz=${128}`;
   const avatar = (
-    <Avatar className="w-4 h-4">
+    <Avatar className="w-4 h-4 flex items-center justify-center">
       <AvatarImage src={faviconFromGoogle} />
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
   );
   return (
-    <span className="inline">
-      <TooltipProvider delayDuration={300}>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <span className="inline-flex justify-center items-center h-[17.5px] w-[17.5px] relative -top-[3px] mx-0.5 pt-0.5 pr-[0.08rem] dark:bg-offsetPlusDark rounded-full text-center">
-              <span className="w-full h-full text-[10px] dark:text-textOffDark dark:hover:text-textMainDark cursor-pointer transition-colors duration-300">
-                {sourceNumber}
-              </span>
+    <TooltipProvider delayDuration={300}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <span className="inline-flex justify-center items-center h-[17.5px] w-[17.5px] relative -top-[3px] mx-0.5 pt-0.5 pr-[0.08rem] dark:bg-offsetPlusDark rounded-full text-center">
+            <span className="w-full h-full text-[10px] dark:text-textOffDark dark:hover:text-rose-100 cursor-pointer transition-colors duration-300">
+              {sourceNumber}
             </span>
-          </TooltipTrigger>
-          <TooltipContent
-            side="bottom"
-            className="dark:bg-contentBackgroundDark dark:border-borderMain/50 shadow-lg rounded-lg  py-[14px] px-4 w-80 h-auto flex flex-col gap-1.5 text-left"
-          >
-            <span className="flex gap-1 items-center">
-              {avatar}
-              <span className="dark:text-textOffDark">
-                {condesedDisplayLink}
-              </span>
+          </span>
+        </TooltipTrigger>
+        <TooltipContent
+          side="bottom"
+          className="dark:bg-contentBackgroundDark dark:border-borderMain/50 shadow-lg rounded-lg  py-[14px] px-4 w-80 h-auto flex flex-col gap-1.5 text-left"
+        >
+          <span className="flex gap-1 items-center w-full">
+            {avatar}
+            <span className="dark:text-textOffDark">{condesedDisplayLink}</span>
+          </span>
+          <a href={link} target={"_blank"}>
+            <span className="text-sm font-medium dark:text-textMainDark h-full dark:hover:text-superDark transition-colors duration-150">
+              {title}
             </span>
-            <a href={link} target={"_blank"}>
-              <span className="text-sm font-medium dark:text-textMainDark h-full dark:hover:text-superDark transition-colors duration-150">
-                {title}
-              </span>
-            </a>
-            <span className="text-sm h-full">{snippet}</span>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-    </span>
+          </a>
+          <span className="text-sm h-full">{snippet}</span>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
