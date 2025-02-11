379b4a7472fae0b44694034c4f946245fa0cbce4
r_hinojosa <r_hinojosa@ucsb.edu>
2025-02-10 20:43:01 -0800

    removed chat from home page

--- components/SearchCard.tsx
+++ components/SearchCard.tsx
@@ -7,13 +7,9 @@ import { useDebounce } from "@/hooks/useDebounce";
 import { useQuery } from "@tanstack/react-query";
 import { fetchSuggestions } from "@/actions/getSuggestions";
 import { useRouter } from "next/navigation";
-import { useChat } from "ai/react";
 
 export default function SearchCard() {
   const router = useRouter();
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: "/api/chatSpeed",
-  });
   const [selectedSuggestionIndex, setSelectedSuggestionIndex] = useState(-1);
   const [displaySuggestions, setDisplaySuggestions] = useState<string[]>([]);
   const [inputFocused, setInputFocused] = useState<boolean>(false);
@@ -98,23 +94,6 @@ export default function SearchCard() {
             ))}
           </div>
         )}
-      <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-        {messages.map((m) => (
-          <div key={m.id} className="whitespace-pre-wrap text-textMainDark">
-            {m.role === "user" ? "User: " : "AI: "}
-            {m.content}
-          </div>
-        ))}
-
-        <form onSubmit={handleSubmit}>
-          <input
-            className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl text-textMainDark"
-            value={input}
-            placeholder="Say something..."
-            onChange={handleInputChange}
-          />
-        </form>
-      </div>
     </div>
   );
 }
