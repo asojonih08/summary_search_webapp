0461fe03babbd5aa662ababef891b4817f0ea47f
asojonih08 <27hinojosa.r@gmail.com>
2025-02-11 17:54:01 -0800

    Added top padding to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SearchCard from "@/components/SearchCard";
 
 export default function Home() {
   return (
-    <div className="w-full min-h-full flex flex-col md:pt-[20vh] pt-[8vh] md:px-0 px-4">
+    <div className="w-full min-h-full flex flex-col md:pt-[20vh] pt-[10vh] md:px-0 px-4">
       <SearchCard />
     </div>
   );
--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 export default function MobileHeader() {
   return (
-    <nav className="fixed top-0 left-0 right-0 h-16 flex items-center mb-6">
+    <nav className="fixed top-0 left-0 right-0 h-16 flex items-center">
       <div className="w-full border-b dark:border-borderMain/50 mx-3.5 h-full flex items-center dark:ring-borderMain/50 dark:divide-borderMain/50 shadow-md">
         <Logo size="large" />
       </div>
